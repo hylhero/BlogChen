0da8a58dd77d9b88d27a7ae72c9566b96884e0e7
cheer <cheerfullchen@outlook.com>
2014-09-29 15:52:31 +0800

    movies

--- interest/admin.py
+++ interest/admin.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from models import ActorModel, AreaModel, MovieStyleModel, MovieModel
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'area', 'img')
+    search_fields = ('name',)
+    list_per_page = 20
+    ordering = ('-createdate',)
+    filter_horizontal = ('style', 'actors')
+
+
+admin.site.register(MovieModel, MovieAdmin)
+admin.site.register(AreaModel)
+admin.site.register(ActorModel)
+admin.site.register(MovieStyleModel)
+
+
--- interest/models.py
+++ interest/models.py
@@ -16,16 +16,20 @@ class MovieStyleModel(models.Model):
         verbose_name_plural = u'电影类型'
 
 class ActorModel(models.Model):
-    name    = models.CharField(u'演员名', max_length=30)
-
+    ACTOR_OR_DIRECTOR = (
+        ('A',u'演员'),
+        ('D',u'导演'),
+    )
+    name    = models.CharField(u'姓名', max_length=30)
+    aord    = models.CharField(u'导演或演员', max_length=1, choices=ACTOR_OR_DIRECTOR, default='A')
     def __unicode__(self):
         return self.name
 
     class Meta:
         ordering            = ['name']
         db_table            = 'interest_actor'
-        verbose_name        = u'演员名'
-        verbose_name_plural = u'演员名'
+        verbose_name        = u'电影演员'
+        verbose_name_plural = u'电影演员'
 
 class AreaModel(models.Model):
     name    = models.CharField(u'地区', max_length=10)
@@ -36,14 +40,24 @@ class AreaModel(models.Model):
     class Meta:
         ordering            = ['name']
         db_table            = 'interest_area'
-        verbose_name        = u'地区'
-        verbose_name_plural = u'地区'
+        verbose_name        = u'电影地区'
+        verbose_name_plural = u'电影地区'
 
 class MovieModel(models.Model):
     name    = models.CharField(u'电影名', max_length=30)
     style   = models.ManyToManyField(MovieStyleModel, blank=False, verbose_name=u'类型')
     area    = models.ForeignKey(AreaModel, null=False, verbose_name=u'地区')
-    direct  = models.ForeignKey(ActorModel, null=False, verbose_name=u'导演')
-    actors  = models.ManyToManyField(ActorModel, blank=False, verbose_name=u'演员')
+    direct  = models.ForeignKey(ActorModel, verbose_name=u'导演', related_name=u'导演')
+    actors  = models.ManyToManyField(ActorModel, verbose_name=u'演员', related_name=u'演员')
     info    = models.TextField(u'简介')
     img     = models.ImageField(upload_to='movies',verbose_name=u'海报',storage=QiniuStorage(folder='movies/'))
+    createdate = models.DateTimeField(u'上传时间',auto_now_add=True,editable=False)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering            = ['name']
+        db_table            = 'interest_moviemodel'
+        verbose_name        = u'电影'
+        verbose_name_plural = u'电影'
